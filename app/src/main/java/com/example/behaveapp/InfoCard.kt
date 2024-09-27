161727180e76361f2b589c19e3679dd1fd95648d
Chloe <cgreenstreet01@gmail.com>
2024-09-27 07:49:02 +0100

    added category to info card data class

--- app/src/main/java/com/example/behaveapp/InfoCard.kt
+++ app/src/main/java/com/example/behaveapp/InfoCard.kt
@@ -2,6 +2,7 @@ package com.example.behaveapp
 data class InfoCard(
     val title: String,
     val country: String,
+    val category: String,
     val description: String,
     val imageUrl: String
 )
